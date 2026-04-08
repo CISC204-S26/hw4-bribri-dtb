43b152a5aa4f989b84a2e09136789ed861e5e370
Flappyow <dtb@udel.edu>
2026-04-08 13:57:28 -0400

    v1

---
+++ pair-project/player.gd
@@ -0,0 +1,24 @@
+extends CharacterBody2D
+
+@export var speed := 200.0
+@export var jump_velocity := -400.0
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func _ready():
+	add_to_group("player")  # for coins and DeathZone detection
+
+func _physics_process(delta):
+	var dir = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	velocity.x = dir * speed
+
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_velocity
+
+	if not is_on_floor():
+		velocity.y += gravity * delta
+		
+
+	move_and_slide()
+
+	if dir != 0 and $Sprite2D:
+		$Sprite2D.flip_h = dir < 0
