7fa0af316adfa08f2b7c10c8afbfd24e26fa463b
Flappyow <dtb@udel.edu>
2026-04-08 14:15:40 -0400

    V3 NO MORE GRAVITY

--- pair-project/player.gd
+++ pair-project/player.gd
@@ -2,20 +2,20 @@ extends CharacterBody2D
 
 @export var speed := 200.0
 @export var jump_velocity := -400.0
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
 
 func _ready():
 	add_to_group("player")  # for coins and DeathZone detection
 
 func _physics_process(delta):
-	var dir = Input.get_action_strength("move right") - Input.get_action_strength("move_left")
+	var dir = Input.get_action_strength("move right") - Input.get_action_strength("move left")
 	velocity.x = dir * speed
 
 	if Input.is_action_just_pressed("move up") and is_on_floor():
 		velocity.y = jump_velocity
 
-	if not is_on_floor():
-		velocity.y += gravity * delta
+
+
 		
 
 	move_and_slide()
